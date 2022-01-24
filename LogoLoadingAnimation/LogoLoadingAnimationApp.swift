f060366addcd758f47b9f3ba5ea1b1d2f5386caf
Nikolaev Vasiliy <vn.nikolaev@vtb.ru>
2022-01-24 21:55:32 +0500

    Initial Commit

---
+++ LogoLoadingAnimation/LogoLoadingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogoLoadingAnimationApp.swift
+//  LogoLoadingAnimation
+//
+//  Created by Васлий Николаев on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct LogoLoadingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
